5a3bd335362f9235d912c686870745e094b95960
Christian Pulmano <cpulmano@ateneo.edu>
2020-03-16 15:17:16 +0800

    Updates

--- modeling.py
+++ modeling.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Logistic and Exponential Modeling for COVID-19
+
+# ### Importing the needed libraries
+
+# In[7]:
+
+
 import pandas as pd
 import numpy as np
 from datetime import datetime,timedelta
@@ -8,23 +18,44 @@ import matplotlib.pyplot as plt
 from IPython import get_ipython
 import requests
 import io
-
+import warnings
+warnings.filterwarnings("ignore")
 ipy = get_ipython()
 if ipy is not None:
     ipy.run_line_magic('matplotlib', 'inline')
 
-url = 'https://raw.githubusercontent.com/cepulmano/covid-modeling/master/covid-19-031520.csv'
+
+# ### Getting the cumulative count dataset
+
+# In[8]:
+
+
+url = 'http://raw.githubusercontent.com/cepulmano/covid-modeling/master/covid-19-031520.csv'
 request = requests.get(url=url, verify=False).content 
 df = pd.read_csv(io.StringIO(request.decode('utf8')))
 
+
+# ### Defining the logistic and exponential model equations
+
+# In[9]:
+
+
 def logistic_model(x,a,b,c):
     return c/(1+np.exp(-(x-b)/a))
+
+def exponential_model(x,a,b,c):
+    return a*np.exp(b*(x-c))
+
+
+# ### Running the logistic and exponential models
+
+# In[10]:
+
 
 x = list(df.iloc[:,0])
 y = list(df.iloc[:,2])
 fit = curve_fit(logistic_model,x,y)
 
-print(fit[0])
 a = fit[0][0] # a refers to the infection speed
 b = fit[0][1] # b is the day with the maximum infections occurred
 c = fit[0][2] # c is the total number of recorded infected people at the infection’s end
@@ -33,9 +64,22 @@ errors = [np.sqrt(fit[1][i][i]) for i in [0,1,2]]
 
 sol = int(fsolve(lambda x : logistic_model(x,a,b,c) - int(c),b))
 
-def exponential_model(x,a,b,c):
-    return a*np.exp(b*(x-c))
+print("Infection speed: {0}".format(a))
+print("Day with maximum infections occured: {0}".format(b))
+print("Total number of record infected people at the infection's end: {0}".format(c))
+print("End of infection in Day #: {0}".format(sol))
+
+
+# In[11]:
+
+
 exp_fit = curve_fit(exponential_model,x,y,p0=[1,1,1], maxfev=5000)
+
+
+# ### Plotting the model outputs
+
+# In[12]:
+
 
 pred_x = list(range(max(x),sol))
 plt.rcParams['figure.figsize'] = [20,7]
@@ -51,3 +95,4 @@ plt.xlabel("Days since 1 January 2020")
 plt.ylabel("Total number of infected people")
 plt.ylim((min(y)*0.9,(c*1.1)))
 plt.show()
+
